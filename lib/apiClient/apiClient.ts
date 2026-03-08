4d8c9d19285610e8211751c7b1f55d8537128b1f
Amit <baghlaamit06@gmail.com>
2026-03-08 18:05:36 +0530

    Fix baseURL type assertion in apiClient

--- lib/apiClient/apiClient.ts
+++ lib/apiClient/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASEURI',
+  baseURL: process.env.NEXT_PUBLIC_BASEURI!',
   withCredentials: true,
 });
